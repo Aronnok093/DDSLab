e8b83a948dd212e06e6a3d310dc4a6abbefad5cd
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2023-07-24 15:17:26 +0600

    updated

--- Project/PC_1/SQL_PL/Table.sql
+++ Project/PC_1/SQL_PL/Table.sql
@@ -4,6 +4,7 @@ DROP TABLE alumni_fragment1 CASCADE CONSTRAINTS;
 DROP TABLE events_fragment1 CASCADE CONSTRAINTS;
 DROP TABLE job_opportunities1 CASCADE CONSTRAINTS;
 DROP TABLE fund_fragment1 CASCADE CONSTRAINTS;
+DROP TABLE alumni_log CASCADE CONSTRAINTS;
 CREATE TABLE alumni_fragment1 (
     alumni_id       NUMBER,
     first_name      VARCHAR2(50),
---
+++ Project/PC_1/SQL_PL/Trigger.sql
@@ -0,0 +1,13 @@
+SET SERVEROUTPUT ON;
+SET VERIFY OFF;
+
+-- Trigger for alumni_fragment1 table
+CREATE OR REPLACE TRIGGER alumni_fragment1_trig
+AFTER INSERT ON alumni_fragment1
+FOR EACH ROW
+BEGIN
+   DBMS_OUTPUT.PUT_LINE('New Alumni Added To Local Server');
+END;
+/
+
+
